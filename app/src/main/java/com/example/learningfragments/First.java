2c3eba02a76556a66c32c6438c99a772fb68d0fb
Teacher <teacher@aptech-msg.edu>
2023-04-29 10:20:58 +0500

    my first android app

---
+++ app/src/main/java/com/example/learningfragments/First.java
@@ -0,0 +1,27 @@
+package com.example.learningfragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class First extends Fragment {
+
+    public First() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/learningfragments/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.learningfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getSupportFragmentManager().beginTransaction().add(R.id.firstSection,new First()).commit();
+
+
+        getSupportFragmentManager().beginTransaction().add(R.id.SecondSection,new Second()).commit();
+
+
+
+
+
+    }
+}
